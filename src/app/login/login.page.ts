ce8e0a6a8dafad92c90324ddd470b87b0d9394f1
Agustin Haller <agustinhaller@gmail.com>
2018-08-23 11:48:30 -0300

    feat(login-page): add login page Just the basics, lots of work TBD

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(public router: Router) {
+    this.loginForm = new FormGroup({
+      email: new FormControl('aa@aa', Validators.required),
+      password: new FormControl('test', Validators.required)
+    });
+  }
 
   ngOnInit() {
+  }
+
+  doLogin(): void {
+    console.log('redirect to home page');
+    // this.router.navigateByUrl('/app/tabs/(home:listing)');
+    this.router.navigate(['app/tabs/', { outlets: {home: [ 'listing' ]} }]);
   }
 
 }
