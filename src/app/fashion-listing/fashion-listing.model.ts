5e299261f3f052217f7cf40bf5743455c373c5de
Agustin Haller <agustinhaller@gmail.com>
2018-11-06 14:47:42 -0300

    feat(fashion): add fashion images and update fashion model

--- src/app/fashion-listing/fashion-listing.model.ts
+++ src/app/fashion-listing/fashion-listing.model.ts
@@ -1,11 +1,15 @@
 export class FashionItemModel {
-  title: string;
-  description: string;
+  price: number;
+  salePrice: number;
   image: string;
+  squareImage: string;
+  showcaseImages: Array<string>;
+  name: string;
+  style: string;
+  relatedProducts: Array<string>;
 }
 
 export class FashionListingModel {
-  title: string;
   items: Array<FashionItemModel> = [
     new FashionItemModel(),
     new FashionItemModel(),
