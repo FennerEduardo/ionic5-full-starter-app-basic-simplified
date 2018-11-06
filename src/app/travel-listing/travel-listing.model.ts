2366a9c9b982c95a57ee0b52a5561f2e3eab60cf
Agustin Haller <agustinhaller@gmail.com>
2018-11-06 14:45:57 -0300

    refactor(travel-listing): remove unused properties

--- src/app/travel-listing/travel-listing.model.ts
+++ src/app/travel-listing/travel-listing.model.ts
@@ -7,18 +7,9 @@ export class TravelItemModel {
   address: string;
   rating: number;
   reviewsCount: number;
-
-  // "picture": "./img/sample-images/travel/Travel1-64.47.png",
-  // "icon": "./img/sample-images/travel/TravelIcon1.png",
-  // "name": "Tristán Narvaja ",
-  // "category": "Flea Market",
-  // "description": "Every Sunday from early morning until three in the afternoon enjoy the biggest market in Montevideo.",
-  // "rating": 4.8,
-  // "address": "Cordón, Montevideo, Uruguay.",
 }
 
 export class TravelListingModel {
-  title: string;
   items: Array<TravelItemModel> = [
     new TravelItemModel(),
     new TravelItemModel(),
