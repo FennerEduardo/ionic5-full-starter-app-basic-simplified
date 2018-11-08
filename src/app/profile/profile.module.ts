f20e27b9fb8e569334f4e4572e7483ced53430ec
Dayana <dayana.jabif@gmail.com>
2018-11-08 16:31:38 -0300

    feat(profile-page): added recent posts section

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -5,12 +5,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { ProfilePage } from './profile.page';
+import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    ComponentsModule,
     RouterModule.forChild([{ path: '', component: ProfilePage }])
   ],
   declarations: [ProfilePage]
