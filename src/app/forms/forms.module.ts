cbeb59afd665b8c3873a5da48a9032723f0a1207
Dayana <dayana.jabif@gmail.com>
2018-12-31 15:20:53 -0300

    feat(forms): add forms and validations page

--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // TODO: move to a shared module instead
 import { Routes, RouterModule } from '@angular/router';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
+import { ComponentsModule } from '../components/components.module';
 import { FormsPage } from './forms.page';
 
 const routes: Routes = [
@@ -18,7 +18,10 @@ const routes: Routes = [
   imports: [
     CommonModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(routes),
+    ComponentsModule
   ],
   declarations: [FormsPage]
 })
--- src/app/forms/forms.page.ts
+++ src/app/forms/forms.page.ts
@@ -1,12 +1,123 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormControl } from '@angular/forms';
+
+import { UsernameValidator } from '../components/validators/username.validator';
+import { PasswordValidator } from '../components/validators/password.validator';
+import { PhoneValidator } from '../components/validators/phone.validator';
+
+import { Country } from './forms.model';
+
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.page.html',
   styleUrls: ['./forms.page.scss'],
 })
-export class FormsPage {
+export class FormsPage implements OnInit {
+
+  validationsForm: FormGroup;
+  matching_passwords_group: FormGroup;
+  country_phone_group: FormGroup;
+  countries: Array<Country>;
+  genders: Array<string>;
+
+  validation_messages = {
+    'username': [
+      { type: 'required', message: 'Username is required.' },
+      { type: 'minlength', message: 'Username must be at least 5 characters long.' },
+      { type: 'maxlength', message: 'Username cannot be more than 25 characters long.' },
+      { type: 'pattern', message: 'Your username must contain only numbers and letters.' },
+      { type: 'validUsername', message: 'Your username has already been taken.' }
+    ],
+    'name': [
+      { type: 'required', message: 'Name is required.' }
+    ],
+    'lastname': [
+      { type: 'required', message: 'Last name is required.' }
+    ],
+    'email': [
+      { type: 'required', message: 'Email is required.' },
+      { type: 'pattern', message: 'Enter a valid email.' }
+    ],
+    'phone': [
+      { type: 'required', message: 'Phone is required.' },
+      { type: 'validCountryPhone', message: 'Phone incorrect for the country selected' }
+    ],
+    'password': [
+      { type: 'required', message: 'Password is required.' },
+      { type: 'minlength', message: 'Password must be at least 5 characters long.' },
+      { type: 'pattern', message: 'Your password must contain at least one uppercase, one lowercase, and one number.' }
+    ],
+    'confirm_password': [
+      { type: 'required', message: 'Confirm password is required' }
+    ],
+    'matching_passwords': [
+      { type: 'areEqual', message: 'Password mismatch' }
+    ],
+    'terms': [
+      { type: 'pattern', message: 'You must accept terms and conditions.' }
+    ],
+  };
 
   constructor() { }
+
+  ngOnInit() {
+    this.countries = [
+      new Country('UY', 'Uruguay'),
+      new Country('US', 'United States'),
+      new Country('AR', 'Argentina')
+    ];
+
+    this.genders = [
+      'Male',
+      'Female'
+    ];
+
+    this.matching_passwords_group = new FormGroup({
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(5),
+        Validators.required,
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+      ])),
+      confirm_password: new FormControl('', Validators.required)
+    }, (formGroup: FormGroup) => {
+      return PasswordValidator.areEqual(formGroup);
+    });
+
+    const country = new FormControl(this.countries[0], Validators.required);
+
+    const phone = new FormControl('', Validators.compose([
+      Validators.required,
+      PhoneValidator.validCountryPhone(country)
+    ]));
+    this.country_phone_group = new FormGroup({
+      country: country,
+      phone: phone
+    });
+
+    this.validationsForm = new FormGroup({
+      'username': new FormControl('', Validators.compose([
+        UsernameValidator.validUsername,
+        Validators.maxLength(25),
+        Validators.minLength(5),
+        Validators.pattern('^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$'),
+        Validators.required
+      ])),
+      'name': new FormControl('', Validators.required),
+      'lastname': new FormControl('', Validators.required),
+      'email': new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      'gender': new FormControl(this.genders[0], Validators.required),
+      'country_phone': this.country_phone_group,
+      'matching_passwords': this.matching_passwords_group,
+      'terms': new FormControl(true, Validators.pattern('true'))
+    });
+  }
+
+  onSubmit(values) {
+    console.log(values);
+  }
 
 }
