052fb16d4e93963ef16b1fea4a5acf0cf77f5b08
Agustin Haller <agustinhaller@gmail.com>
2019-01-24 09:48:30 -0300

    refactor(counter-input): refactor counter input component TODO: extract the validator from the component.ts file to its own file

--- src/app/components/counter-input/counter-input.component.ts
+++ src/app/components/counter-input/counter-input.component.ts
@@ -1,8 +1,6 @@
 import { Component, forwardRef, Input, OnChanges, ViewEncapsulation } from '@angular/core';
 import { FormControl, ControlValueAccessor, NG_VALUE_ACCESSOR, NG_VALIDATORS } from '@angular/forms';
 
-const noop = () => {};
-
 export function counterRangeValidator(maxValue, minValue) {
   return (c: FormControl) => {
     const err = {
@@ -18,12 +16,9 @@ export function counterRangeValidator(maxValue, minValue) {
 }
 
 @Component({
-  selector: 'counter-input',
+  selector: 'app-counter-input',
   templateUrl: './counter-input.component.html',
   styleUrls: ['./counter-input.component.scss'],
-  // host: {
-  //   'class': 'counter-input'
-  // },
   providers: [
     { provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => CounterInputComponent), multi: true },
     { provide: NG_VALIDATORS, useExisting: forwardRef(() => CounterInputComponent), multi: true }
@@ -31,15 +26,15 @@ export function counterRangeValidator(maxValue, minValue) {
   encapsulation: ViewEncapsulation.None
 })
 export class CounterInputComponent implements ControlValueAccessor, OnChanges {
-  propagateChange: any = noop;
-  validateFn: any = noop;
-
   // tslint:disable-next-line:no-input-rename
   @Input('counterValue') _counterValue = 0;
   // tslint:disable-next-line:no-input-rename
   @Input('max') counterRangeMax;
   // tslint:disable-next-line:no-input-rename
   @Input('min') counterRangeMin;
+
+  propagateChange: any = () => {}; // Noop function
+  validateFn: any = () => {}; // Noop function
 
   get counterValue() {
     return this._counterValue;
