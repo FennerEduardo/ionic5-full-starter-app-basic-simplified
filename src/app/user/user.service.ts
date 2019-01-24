8a132b1356c2f2e1d25f8e81ab9fdfc75ef820e1
Dayana <dayana.jabif@gmail.com>
2019-01-24 09:52:08 -0300

    refactor(user-service): fix type error

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -10,35 +10,36 @@ import { UserFriendsModel } from './user-friends/user-friends.model';
 
 @Injectable()
 export class UserService {
-  private _dataCache: SubjectFetch<UserProfileModel>;
+  private _profileDataCache: SubjectFetch<UserProfileModel>;
+  private _friendsDataCache: SubjectFetch<UserFriendsModel>;
 
   constructor(private http: HttpClient) { }
 
   public getProfileData(): Observable<UserProfileModel> {
     // Use cache if we have it.
-    if (!this._dataCache) {
+    if (!this._profileDataCache) {
       // Initialize the model specifying that it is a shell model
       const dataShell: UserProfileModel = new UserProfileModel(true);
-      this._dataCache = new SubjectFetch(
+      this._profileDataCache = new SubjectFetch(
         dataShell,
         () => this.http.get<UserProfileModel>('./assets/sample-data/user/user-profile.json')
       );
     }
 
-    return this._dataCache.observable;
+    return this._profileDataCache.observable;
   }
 
   public getFriendsData(): Observable<UserFriendsModel> {
     // Use cache if we have it.
-    if (!this._dataCache) {
+    if (!this._friendsDataCache) {
       // Initialize the model specifying that it is a shell model
-      const dataShell: UserProfileModel = new UserFriendsModel(true);
-      this._dataCache = new SubjectFetch(
+      const dataShell: UserFriendsModel = new UserFriendsModel(true);
+      this._friendsDataCache = new SubjectFetch(
         dataShell,
-        () => this.http.get<UserProfileModel>('./assets/sample-data/user/user-friends.json')
+        () => this.http.get<UserFriendsModel>('./assets/sample-data/user/user-friends.json')
       );
     }
 
-    return this._dataCache.observable;
+    return this._friendsDataCache.observable;
   }
 }
