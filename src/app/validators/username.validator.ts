cc5c3ec885d14813e0884ab9eb753ba0267353be
Dayana <dayana.jabif@gmail.com>
2019-02-06 10:52:26 -0300

    refactor(username validator): typos

--- src/app/validators/username.validator.ts
+++ src/app/validators/username.validator.ts
@@ -5,7 +5,7 @@ export class UsernameValidator {
   static usernameNotAvailable(fc: FormControl) {
 
     // this is a dummy validator to check if the username is valid or not.
-    // on a real app you should check against your DB if the username is not already in use.
+    // In a real app you should check against your DB if the username is already in use.
     // in this example we define two existing usernames: 'abc123' and '123abc'
     // If our validation fails, we return an object with a key for the error name and a value of true.
     // Otherwise, if the validation passes, we simply return null because there is no error.
