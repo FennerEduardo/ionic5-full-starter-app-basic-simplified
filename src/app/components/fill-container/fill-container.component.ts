d2b0eccf3542d95cbf3551d9ce258bebc5682d78
Agustin Haller <agustinhaller@gmail.com>
2019-02-15 16:55:27 -0300

    refactor(fill-container): use HostBinding instead of Renderer2

--- src/app/components/fill-container/fill-container.component.ts
+++ src/app/components/fill-container/fill-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ElementRef, Renderer2, ViewEncapsulation, OnChanges } from '@angular/core';
+import { Component, Input, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-fill-container',
@@ -7,18 +7,19 @@ import { Component, Input, ElementRef, Renderer2, ViewEncapsulation, OnChanges }
     './fill-container.component.scss'
   ]
 })
-export class FillContainerComponent implements OnChanges {
+export class FillContainerComponent {
   _ratio: { w: number, h: number };
 
-  constructor(private _elementRef: ElementRef, private _renderer: Renderer2) {}
+  @HostBinding('style.padding') ratioPadding = '0px';
 
-  @Input() set ratio(ratio: { w: number, h: number }) {
-    this._ratio = ratio || {w: 1, h: 1};
-  }
+  @Input()
+  set ratio(ratio: { w: number, h: number }) {
+    this._ratio = (ratio !== undefined && ratio !== null) ? ratio : {w: 1, h: 1};
 
-  ngOnChanges() {
     const ratio_height = (this._ratio.h / this._ratio.w * 100) + '%';
     // Conserve aspect ratio (see: http://stackoverflow.com/a/10441480/1116959)
-    this._renderer.setStyle(this._elementRef.nativeElement, 'padding', '0px 0px ' + ratio_height + ' 0px');
+    this.ratioPadding = '0px 0px ' + ratio_height + ' 0px';
   }
+
+  constructor() { }
 }
