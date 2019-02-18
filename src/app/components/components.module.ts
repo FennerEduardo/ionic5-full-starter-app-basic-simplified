345573895f0085a6bd288f59df92890e492b1d34
Agustin Haller <agustinhaller@gmail.com>
2019-02-18 12:05:54 -0300

    refactor(color-radio): replace this directive with a css 4 alternative

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,18 +5,18 @@ import { IonicModule } from '@ionic/angular';
 
 import { CustomCheckboxComponent } from './custom-checkbox/custom-checkbox.component';
 import { ShowHidePasswordComponent } from './show-hide-password/show-hide-password.component';
+
 import { FillContainerComponent } from './fill-container/fill-container.component';
+import { PreloadImageComponent } from './preload-image/preload-image.component';
+import { BackgroundImageComponent } from './background-image/background-image.component';
 
 import { AspectRatioComponent } from './aspect-ratio/aspect-ratio.component';
 import { TextShellComponent } from './text-shell/text-shell.component';
 import { ImageShellComponent } from './image-shell/image-shell.component';
 
-import { PreloadImageComponent } from './preload-image/preload-image.component';
-import { BackgroundImageComponent } from './background-image/background-image.component';
 import { CountdownTimerComponent } from './countdown-timer/countdown-timer.component';
 import { CounterInputComponent } from './counter-input/counter-input.component';
 import { RatingInputComponent } from './rating-input/rating-input.component';
-import { ColorRadioDirective } from './color-radio/color-radio.directive';
 
 @NgModule({
   imports: [
@@ -27,34 +27,34 @@ import { ColorRadioDirective } from './color-radio/color-radio.directive';
   declarations: [
     CustomCheckboxComponent,
     ShowHidePasswordComponent,
+
     FillContainerComponent,
+    PreloadImageComponent,
+    BackgroundImageComponent,
 
     AspectRatioComponent,
     TextShellComponent,
     ImageShellComponent,
 
-    PreloadImageComponent,
-    BackgroundImageComponent,
     CountdownTimerComponent,
     CounterInputComponent,
-    RatingInputComponent,
-    ColorRadioDirective
+    RatingInputComponent
   ],
   exports: [
     CustomCheckboxComponent,
     ShowHidePasswordComponent,
+
     FillContainerComponent,
+    PreloadImageComponent,
+    BackgroundImageComponent,
 
     AspectRatioComponent,
     TextShellComponent,
     ImageShellComponent,
 
-    PreloadImageComponent,
-    BackgroundImageComponent,
     CountdownTimerComponent,
     CounterInputComponent,
-    RatingInputComponent,
-    ColorRadioDirective
+    RatingInputComponent
   ],
   entryComponents: [],
 })
