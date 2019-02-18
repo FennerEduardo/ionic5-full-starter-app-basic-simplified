45499c38454aa3103c8ee094e1765b75aac4016b
Agustin Haller <agustinhaller@gmail.com>
2019-02-18 15:47:14 -0300

    refactor(showcase): add model to showcase service

--- src/app/showcase/showcase.service.ts
+++ src/app/showcase/showcase.service.ts
@@ -5,26 +5,22 @@ import { Observable } from 'rxjs';
 
 import { SubjectFetch } from '../utils/subject-fetch';
 
-// import { TravelListingModel } from './listing/travel-listing.model';
+import { ShowcaseShellModel } from './models/showcase-shell.model';
 
 @Injectable()
 export class ShowcaseService {
-  // private _detailsCache: SubjectFetch<TravelDetailsModel>;
-  private _detailsCache: SubjectFetch<any>;
+  private _detailsCache: SubjectFetch<ShowcaseShellModel>;
 
   constructor(private http: HttpClient) { }
 
-  // public details(): Observable<TravelListingModel> {
-  public shellDetails(): Observable<any> {
+  public shellDetails(): Observable<ShowcaseShellModel> {
     // Use cache if we have it.
     if (!this._detailsCache) {
       // Initialize the model specifying that it is a shell model
-      // const detailsShell: TravelListingModel = new TravelListingModel(true);
-      const detailsShell: { title: string } = { title: null };
+      const detailsShell: ShowcaseShellModel = new ShowcaseShellModel(true);
       this._detailsCache = new SubjectFetch(
         detailsShell,
-        // () => this.http.get<TravelListingModel>('./assets/sample-data/travel/details.json')
-        () => this.http.get<{ title: string }>('./assets/sample-data/showcase/shell.json')
+        () => this.http.get<ShowcaseShellModel>('./assets/sample-data/showcase/shell.json')
       );
     }
 
