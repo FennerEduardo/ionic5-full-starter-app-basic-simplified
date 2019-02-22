10209bfc0da3b76f398b464672b2f8e7f9c7f1bf
Agustin Haller <agustinhaller@gmail.com>
2019-02-22 17:25:22 -0300

    refactor(travel): clean code

--- src/app/travel/details/travel-details.page.ts
+++ src/app/travel/details/travel-details.page.ts
@@ -10,32 +10,44 @@ import { TravelDetailsModel } from './travel-details.model';
 })
 export class TravelDetailsPage implements OnInit {
   details: TravelDetailsModel;
+
   @HostBinding('class.is-shell') get isShell() {
     return (this.details && this.details.isShell) ? true : false;
   }
 
   constructor(private route: ActivatedRoute) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     if (this.route && this.route.data) {
-      this.route.data.subscribe(routeData => {
-        const data = routeData['data'];
-        console.log('data - TravelDetailsPage - ngOnInit()', data);
+      // We resolved a promise for the data Observable
+      const promiseObservable = this.route.data;
+      console.log('Route Resolve Observable => promiseObservable: ', promiseObservable);
 
-        if (data) {
-          data.subscribe((observableData: TravelDetailsModel) => {
-            if (observableData) {
-              this.details = observableData;
-            }
+      if (promiseObservable) {
+        promiseObservable.subscribe(promiseValue => {
+          const dataObservable = promiseValue['data'];
+          console.log('Subscribe to promiseObservable => dataObservable: ', dataObservable);
 
-            console.log('observableData - TravelDetailsPage - ngOnInit()', observableData);
-          });
-        } else {
-          alert('No route data 2');
-        }
-      });
+          if (dataObservable) {
+            dataObservable.subscribe(observableValue => {
+              const pageData: TravelDetailsModel = observableValue;
+              // tslint:disable-next-line:max-line-length
+              console.log('Subscribe to dataObservable (can emmit multiple values) => PageData (' + ((pageData && pageData.isShell) ? 'SHELL' : 'REAL') + '): ', pageData);
+              // As we are implementing an App Shell architecture, pageData will be firstly an empty shell model,
+              // and the real remote data once it gets fetched
+              if (pageData) {
+                this.details = pageData;
+              }
+            });
+          } else {
+            console.warn('No dataObservable coming from Route Resolver promiseObservable');
+          }
+        });
+      } else {
+        console.warn('No promiseObservable coming from Route Resolver data');
+      }
     } else {
-      alert('No route data');
+      console.warn('No data coming from Route Resolver');
     }
   }
 }
--- src/app/travel/listing/travel-listing.page.ts
+++ src/app/travel/listing/travel-listing.page.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { delay } from 'rxjs/operators';
-
 import { TravelListingModel } from './travel-listing.model';
 
 @Component({
@@ -13,30 +11,39 @@ import { TravelListingModel } from './travel-listing.model';
 export class TravelListingPage implements OnInit {
   listing: TravelListingModel;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute) { }
 
-  }
-
-  ngOnInit() {
+  ngOnInit(): void {
     if (this.route && this.route.data) {
-      this.route.data.subscribe(routeData => {
-        const data = routeData['data'];
-        console.log('data - TravelListingPage - ngOnInit()', data);
+      // We resolved a promise for the data Observable
+      const promiseObservable = this.route.data;
+      console.log('Route Resolve Observable => promiseObservable: ', promiseObservable);
 
-        if (data) {
-          data.subscribe((observableData: TravelListingModel) => {
-            if (observableData && observableData.items) {
-              this.listing = observableData;
-            }
+      if (promiseObservable) {
+        promiseObservable.subscribe(promiseValue => {
+          const dataObservable = promiseValue['data'];
+          console.log('Subscribe to promiseObservable => dataObservable: ', dataObservable);
 
-            console.log('observableData - TravelListingPage - ngOnInit()', observableData);
-          });
-        } else {
-          alert('No route data 2');
-        }
-      });
+          if (dataObservable) {
+            dataObservable.subscribe(observableValue => {
+              const pageData: TravelListingModel = observableValue;
+              // tslint:disable-next-line:max-line-length
+              console.log('Subscribe to dataObservable (can emmit multiple values) => PageData (' + ((pageData && pageData.isShell) ? 'SHELL' : 'REAL') + '): ', pageData);
+              // As we are implementing an App Shell architecture, pageData will be firstly an empty shell model,
+              // and the real remote data once it gets fetched
+              if (pageData) {
+                this.listing = pageData;
+              }
+            });
+          } else {
+            console.warn('No dataObservable coming from Route Resolver promiseObservable');
+          }
+        });
+      } else {
+        console.warn('No promiseObservable coming from Route Resolver data');
+      }
     } else {
-      alert('No route data');
+      console.warn('No data coming from Route Resolver');
     }
   }
 }
