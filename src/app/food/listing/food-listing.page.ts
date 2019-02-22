fac1a0be467be703225179adc2fbd446570c74db
Agustin Haller <agustinhaller@gmail.com>
2019-02-22 16:54:13 -0300

    refactor(food): clean code

--- src/app/food/listing/food-listing.page.ts
+++ src/app/food/listing/food-listing.page.ts
@@ -11,30 +11,39 @@ import { FoodListingModel } from './food-listing.model';
 export class FoodListingPage implements OnInit {
   listing: FoodListingModel;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute) { }
 
-  }
-
-  ngOnInit() {
+  ngOnInit(): void {
     if (this.route && this.route.data) {
-      this.route.data.subscribe(routeData => {
-        const data = routeData['data'];
-        console.log('data - FoodListingPage - ngOnInit()', data);
+      // We resolved a promise for the data Observable
+      const promiseObservable = this.route.data;
+      console.log('Route Resolve Observable => promiseObservable: ', promiseObservable);
 
-        if (data) {
-          data.subscribe((observableData: FoodListingModel) => {
-            if (observableData && observableData.items) {
-              this.listing = observableData;
-            }
+      if (promiseObservable) {
+        promiseObservable.subscribe(promiseValue => {
+          const dataObservable = promiseValue['data'];
+          console.log('Subscribe to promiseObservable => dataObservable: ', dataObservable);
 
-            console.log('observableData - FoodListingPage - ngOnInit()', observableData);
-          });
-        } else {
-          alert('No route data 2');
-        }
-      });
+          if (dataObservable) {
+            dataObservable.subscribe(observableValue => {
+              const pageData: FoodListingModel = observableValue;
+              // tslint:disable-next-line:max-line-length
+              console.log('Subscribe to dataObservable (can emmit multiple values) => PageData (' + ((pageData && pageData.isShell) ? 'SHELL' : 'REAL') + '): ', pageData);
+              // As we are implementing an App Shell architecture, pageData will be firstly an empty shell model,
+              // and the real remote data once it gets fetched
+              if (pageData) {
+                this.listing = pageData;
+              }
+            });
+          } else {
+            console.warn('No dataObservable coming from Route Resolver promiseObservable');
+          }
+        });
+      } else {
+        console.warn('No promiseObservable coming from Route Resolver data');
+      }
     } else {
-      alert('No route data');
+      console.warn('No data coming from Route Resolver');
     }
   }
 }
