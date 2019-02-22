c22b55655323bfe21281b26c57c4a91d1e8fc572
Agustin Haller <agustinhaller@gmail.com>
2019-02-22 17:18:03 -0300

    refactor(real-state): clean code

--- src/app/real-state/details/real-state-details.page.ts
+++ src/app/real-state/details/real-state-details.page.ts
@@ -10,32 +10,44 @@ import { RealStateDetailsModel } from './real-state-details.model';
 })
 export class RealStateDetailsPage implements OnInit {
   details: RealStateDetailsModel;
+
   @HostBinding('class.is-shell') get isShell() {
     return (this.details && this.details.isShell) ? true : false;
   }
 
   constructor(private route: ActivatedRoute) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     if (this.route && this.route.data) {
-      this.route.data.subscribe(routeData => {
-        const data = routeData['data'];
-        console.log('data - RealStateDetailsPage - ngOnInit()', data);
+      // We resolved a promise for the data Observable
+      const promiseObservable = this.route.data;
+      console.log('Route Resolve Observable => promiseObservable: ', promiseObservable);
 
-        if (data) {
-          data.subscribe((observableData: RealStateDetailsModel) => {
-            if (observableData) {
-              this.details = observableData;
-            }
+      if (promiseObservable) {
+        promiseObservable.subscribe(promiseValue => {
+          const dataObservable = promiseValue['data'];
+          console.log('Subscribe to promiseObservable => dataObservable: ', dataObservable);
 
-            console.log('observableData - RealStateDetailsPage - ngOnInit()', observableData);
-          });
-        } else {
-          alert('No route data 2');
-        }
-      });
+          if (dataObservable) {
+            dataObservable.subscribe(observableValue => {
+              const pageData: RealStateDetailsModel = observableValue;
+              // tslint:disable-next-line:max-line-length
+              console.log('Subscribe to dataObservable (can emmit multiple values) => PageData (' + ((pageData && pageData.isShell) ? 'SHELL' : 'REAL') + '): ', pageData);
+              // As we are implementing an App Shell architecture, pageData will be firstly an empty shell model,
+              // and the real remote data once it gets fetched
+              if (pageData) {
+                this.details = pageData;
+              }
+            });
+          } else {
+            console.warn('No dataObservable coming from Route Resolver promiseObservable');
+          }
+        });
+      } else {
+        console.warn('No promiseObservable coming from Route Resolver data');
+      }
     } else {
-      alert('No route data');
+      console.warn('No data coming from Route Resolver');
     }
   }
 }
--- src/app/real-state/listing/real-state-listing.page.ts
+++ src/app/real-state/listing/real-state-listing.page.ts
@@ -10,34 +10,46 @@ import { RealStateListingModel } from './real-state-listing.model';
 })
 export class RealStateListingPage implements OnInit {
   listing: RealStateListingModel;
+
   // You can add a class to know when the shell has finish loading to the entire page or by using [ngClass] in the html
   //    <ion-content [ngClass]="{'is-shell': (listing.isShell)}">
   @HostBinding('class.is-shell') get isShell() {
     return (this.listing && this.listing.isShell) ? true : false;
-}
+  }
 
   constructor(private route: ActivatedRoute) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     if (this.route && this.route.data) {
-      this.route.data.subscribe(routeData => {
-        const data = routeData['data'];
-        console.log('data - RealStateListingPage - ngOnInit()', data);
+      // We resolved a promise for the data Observable
+      const promiseObservable = this.route.data;
+      console.log('Route Resolve Observable => promiseObservable: ', promiseObservable);
 
-        if (data) {
-          data.subscribe((observableData: RealStateListingModel) => {
-            if (observableData && observableData.items) {
-              this.listing = observableData;
-            }
+      if (promiseObservable) {
+        promiseObservable.subscribe(promiseValue => {
+          const dataObservable = promiseValue['data'];
+          console.log('Subscribe to promiseObservable => dataObservable: ', dataObservable);
 
-            console.log('observableData - RealStateListingPage - ngOnInit()', observableData);
-          });
-        } else {
-          alert('No route data 2');
-        }
-      });
+          if (dataObservable) {
+            dataObservable.subscribe(observableValue => {
+              const pageData: RealStateListingModel = observableValue;
+              // tslint:disable-next-line:max-line-length
+              console.log('Subscribe to dataObservable (can emmit multiple values) => PageData (' + ((pageData && pageData.isShell) ? 'SHELL' : 'REAL') + '): ', pageData);
+              // As we are implementing an App Shell architecture, pageData will be firstly an empty shell model,
+              // and the real remote data once it gets fetched
+              if (pageData) {
+                this.listing = pageData;
+              }
+            });
+          } else {
+            console.warn('No dataObservable coming from Route Resolver promiseObservable');
+          }
+        });
+      } else {
+        console.warn('No promiseObservable coming from Route Resolver data');
+      }
     } else {
-      alert('No route data');
+      console.warn('No data coming from Route Resolver');
     }
   }
 }
