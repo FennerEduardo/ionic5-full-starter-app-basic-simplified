510c611faf6c33d1ef4db2b7894cf86c19b90d4e
Agustin Haller <agustinhaller@gmail.com>
2019-02-26 15:45:15 -0300

    refactor(login): update variables, clean code

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -6,7 +6,9 @@ import { MenuController } from '@ionic/angular';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: [
+    './styles/login.page.scss'
+  ]
 })
 export class LoginPage implements OnInit {
   loginForm: FormGroup;
