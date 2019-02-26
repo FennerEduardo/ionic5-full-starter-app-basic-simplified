6d3206be3052cb12ffb9d62e68eb6d283427b1b2
Agustin Haller <agustinhaller@gmail.com>
2019-02-26 17:34:58 -0300

    refactor(categories): clean code, add shell and responsive styles

--- src/app/categories/categories.page.ts
+++ src/app/categories/categories.page.ts
@@ -3,6 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.page.html',
-  styleUrls: ['./categories.page.scss']
+  styleUrls: [
+    './styles/categories.page.scss',
+    './styles/categories.shell.scss',
+    './styles/categories.responsive.scss'
+  ]
 })
 export class CategoriesPage { }
