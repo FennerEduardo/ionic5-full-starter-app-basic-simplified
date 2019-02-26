9c421f37a877785951c7295f55c0587d50efd91b
Agustin Haller <agustinhaller@gmail.com>
2019-02-26 18:39:03 -0300

    refactor(contact-card): clean code, add shell styles

--- src/app/contact-card/contact-card.page.ts
+++ src/app/contact-card/contact-card.page.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact-card',
   templateUrl: './contact-card.page.html',
-  styleUrls: ['./contact-card.page.scss']
+  styleUrls: [
+    './styles/contact-card.page.scss',
+    './styles/contact-card.shell.scss'
+  ]
 })
-export class ContactCardPage {}
+export class ContactCardPage { }
