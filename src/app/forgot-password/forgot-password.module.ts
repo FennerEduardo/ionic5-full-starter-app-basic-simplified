7ecd3858e547c34ccd168ebfe3f4912e41d4ee46
Dayana <dayana.jabif@gmail.com>
2019-05-13 10:00:15 -0300

    refactor: remove // TODO: move to a shared module instead

--- src/app/forgot-password/forgot-password.module.ts
+++ src/app/forgot-password/forgot-password.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// TODO: move to a shared module instead
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
--- src/app/forms/validations/forms-validations.module.ts
+++ src/app/forms/validations/forms-validations.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// TODO: move to a shared module instead
 import { Routes, RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
