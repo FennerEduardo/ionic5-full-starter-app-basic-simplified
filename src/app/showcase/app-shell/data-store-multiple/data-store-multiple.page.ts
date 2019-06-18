879a03ae2f1be6d23e4544c746c03d331d2e1ca8
Agustin Haller <agustinhaller@gmail.com>
2019-06-18 14:45:00 -0300

    feat(data-store-multiple-binding): add scaffolding

---
+++ src/app/showcase/app-shell/data-store-multiple/data-store-multiple.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-store-multiple',
+  templateUrl: './data-store-multiple.page.html',
+  styleUrls: ['./data-store-multiple.page.scss'],
+})
+export class DataStoreMultiplePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
