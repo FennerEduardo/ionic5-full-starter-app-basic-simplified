779b823b6530f64392f12f202711f816c8eb5a4a
Agustin Haller <agustinhaller@gmail.com>
2019-06-18 14:31:41 -0300

    feat(data-store-list-binding): add scaffold

---
+++ src/app/showcase/app-shell/data-store-list/data-store-list.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-store-list',
+  templateUrl: './data-store-list.page.html',
+  styleUrls: ['./data-store-list.page.scss'],
+})
+export class DataStoreListPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
