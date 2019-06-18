e97eebd40809c43f52ebf2ad1c275fe4bdb9a245
Agustin Haller <agustinhaller@gmail.com>
2019-06-18 14:07:25 -0300

    refactor(text-shell-showcase): move to separate page

---
+++ src/app/showcase/app-shell/text-shell/text-shell.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-shell-page',
+  templateUrl: './text-shell.page.html',
+  styleUrls: ['./text-shell.page.scss'],
+})
+export class TextShellPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
