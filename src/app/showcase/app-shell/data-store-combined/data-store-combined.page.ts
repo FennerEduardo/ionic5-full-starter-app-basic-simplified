9f9e9d1257fe2464645fb9f20abdb50a2e5f3894
Agustin Haller <agustinhaller@gmail.com>
2019-06-18 14:42:56 -0300

    feat(data-store-combined-binding): add scaffolding

---
+++ src/app/showcase/app-shell/data-store-combined/data-store-combined.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-store-combined',
+  templateUrl: './data-store-combined.page.html',
+  styleUrls: ['./data-store-combined.page.scss'],
+})
+export class DataStoreCombinedPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
