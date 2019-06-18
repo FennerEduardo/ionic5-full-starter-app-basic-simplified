9d0caa2377af999799a8d29f69230556c7fecd79
Agustin Haller <agustinhaller@gmail.com>
2019-06-18 14:38:49 -0300

    feat(data-store-subset-binding): add scaffolding

---
+++ src/app/showcase/app-shell/data-store-subset/data-store-subset.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-store-subset',
+  templateUrl: './data-store-subset.page.html',
+  styleUrls: ['./data-store-subset.page.scss'],
+})
+export class DataStoreSubsetPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
