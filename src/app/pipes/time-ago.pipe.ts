62a659225c4ea34566e5592ee61e0b31199e71f6
Agustin <agustinhaller@gmail.com>
2020-05-07 17:34:23 -0300

    fix(dayjs): no need to use the workaround anymore Latest version of the dayjs lib fixed this

--- src/app/pipes/time-ago.pipe.ts
+++ src/app/pipes/time-ago.pipe.ts
@@ -1,15 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-// When fixed, uncomment this
-// import * as dayjs from 'dayjs';
-import * as _dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
-
-// TODO:  dayjs is throiwing this ERROR:
-//        error TS2339: Property 'to' does not exist on type 'Dayjs'.
-// Luckily there is a PR which solves it (https://github.com/iamkun/dayjs/issues/297#issuecomment-442748858)
-// When fixed, remove this line:
-const dayjs: any = _dayjs;
+import * as dayjs from 'dayjs';
+import * as relativeTime from 'dayjs/plugin/relativeTime';
 
 @Pipe({ name: 'appTimeAgo' })
 export class TimeAgoPipe implements PipeTransform {
