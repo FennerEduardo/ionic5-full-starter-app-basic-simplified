0deabd065470393238d8afca2a31f90ae4aa4504
Agustin <agustinhaller@gmail.com>
2020-09-18 11:49:36 -0300

    fix: add http interceptor

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
+import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { UniversalInterceptor } from './universal-interceptor';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
+    ServerTransferStateModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: UniversalInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent],
 })
